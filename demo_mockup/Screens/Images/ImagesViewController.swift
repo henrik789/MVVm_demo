cd8b80bf66d1a57850cd376833b32ed8d81f6ace
Henrik <henrik789@gmail.com>
2019-04-17 16:14:49 +0200

    Added nice button with shadow effect

--- demo_mockup/Screens/Images/ImagesViewController.swift
+++ demo_mockup/Screens/Images/ImagesViewController.swift
@@ -4,33 +4,40 @@ import UIKit
 
 class ImagesViewController: UIViewController {
     
-    
-    
     let viewDemo = UIView()
-    
-    @IBAction func imgeButton(_ sender: Any) {
-        self.view.addSubview(viewDemo)
-        UIView.animate(withDuration: 1.0, delay: 0, options: [.repeat, .autoreverse, .transitionFlipFromBottom, .curveEaseInOut], animations: {
-            self.viewDemo.alpha = 0
-            self.viewDemo.frame = CGRect(x: 50, y: 350, width: 120, height: 120)
-        }, completion: nil)
-        
-    }
+    var screenWidth = CGFloat()
+    var screenHeight = CGFloat()
+
+    @IBOutlet weak var button1: PushButton!
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        screenWidth = self.view.frame.size.width
+        screenHeight = self.view.frame.size.height
         config()
     }
     
     func config(){
-        let screenWidth = self.view.frame.size.width
-        let screenHeight = self.view.frame.size.height
+        button1.layer.shadowColor = UIColor.black.cgColor
+        button1.layer.shadowOffset = CGSize(width: 1, height:3)
+        button1.layer.shadowRadius = 3
+        button1.layer.shadowOpacity = 0.6
+
         viewDemo.backgroundColor = UIColor.yellow
-        viewDemo.frame = CGRect(x: screenWidth / 2, y: screenHeight / 2, width: 100, height: 100)
-        viewDemo.layer.cornerRadius = 50
+        viewDemo.frame = CGRect(x: (screenWidth / 2) - 30, y: (screenHeight / 2) - 30, width: 60, height: 60)
+//        viewDemo.layer.cornerRadius = 30
         viewDemo.alpha = 1
         print(screenWidth, screenHeight)
     }
-
+    
+    @IBAction func imgeButton(_ sender: PushButton) {
+        self.view.addSubview(viewDemo)
+        UIView.animate(withDuration: 1.3, delay: 0, options: [.repeat, .autoreverse, .curveEaseInOut], animations: {
+            self.viewDemo.alpha = 1
+            self.viewDemo.backgroundColor = UIColor.red
+            self.viewDemo.frame = CGRect(x: 0, y: 0, width: self.screenWidth, height: self
+            .screenHeight)
+        }, completion: nil)
+    }
+    
 }
 
